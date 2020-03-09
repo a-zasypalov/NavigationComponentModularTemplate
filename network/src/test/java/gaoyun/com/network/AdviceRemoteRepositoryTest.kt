0c237f79c266224532c83f6fae9a5954e1003930
Artem Zasypalov <a.zasypalov@kingbird.ru>
2020-03-09 08:03:44 +0400

    Add repository tests

--- network/src/test/java/gaoyun/com/network/AdviceRemoteRepositoryTest.kt
+++ network/src/test/java/gaoyun/com/network/AdviceRemoteRepositoryTest.kt
@@ -5,6 +5,8 @@ import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import gaoyun.com.network.repository.AdviceRemoteRepositoryImpl
 import gaoyun.com.network.responses.AdviceObject
+import gaoyun.com.network.responses.ErrorMessage
+import gaoyun.com.network.responses.ErrorResponse
 import gaoyun.com.network.responses.SlipObject
 import io.reactivex.Single
 import org.junit.Test
@@ -16,13 +18,33 @@ class AdviceRemoteRepositoryTest {
     private val repository = AdviceRemoteRepositoryImpl(adviceServiceMock)
 
     @Test
-    fun getRandomAdviceFromRepository_EmitsAdvice() {
+    fun getRandomAdvice_EmitsAdvice() {
         val advice = AdviceObject(SlipObject(faker.idNumber().valid(), faker.lorem().sentence()))
 
         whenever(adviceServiceMock.getRandomAdvice()).thenReturn(Single.just(advice))
 
         val testObserver = repository.getRandomAdvice().test()
         testObserver.assertValue(advice)
+    }
+
+    @Test
+    fun getAdviceByIdWithValidId_EmitsAdvice() {
+        val advice = AdviceObject(SlipObject(faker.idNumber().valid(), faker.lorem().sentence()))
+
+        whenever(adviceServiceMock.getAdviceById(advice.slip.slipId)).thenReturn(Single.just(advice))
+
+        val testObserver = repository.getRandomAdvice().test()
+        testObserver.assertValue(advice)
+    }
+
+    @Test
+    fun getAdviceByIdWithNotValidId_EmitsError() {
+        val error = ErrorResponse(ErrorMessage(faker.lorem().word(), faker.lorem().sentence()))
+
+        whenever(adviceServiceMock.getAdviceById(faker.idNumber().invalid())).thenReturn(Single.just(error))
+
+        val testObserver = repository.getRandomAdvice().test()
+        testObserver.assertValue(error)
     }
 
 }
