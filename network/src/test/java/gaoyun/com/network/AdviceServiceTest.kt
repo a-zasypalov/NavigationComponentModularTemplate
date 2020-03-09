73c3b768c1ae7e9b3993f224ded6a21b4e43e151
Artem Zasypalov <a.zasypalov@kingbird.ru>
2020-03-09 08:19:08 +0400

    Add service tests

--- network/src/test/java/gaoyun/com/network/AdviceServiceTest.kt
+++ network/src/test/java/gaoyun/com/network/AdviceServiceTest.kt
@@ -1,6 +1,7 @@
 package gaoyun.com.network
 
 import gaoyun.com.network.responses.AdviceObject
+import gaoyun.com.network.responses.ErrorMessage
 import gaoyun.com.network.responses.SlipObject
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
@@ -11,8 +12,14 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 private const val id = "109"
+private const val invalidId = "0"
+
 private const val advice = "Advice"
 private const val testJson = "{\"slip\": {\"advice\":\"$advice\",\"slip_id\":\"$id\"}}"
+
+private const val errorType = "error"
+private const val errorText = "Advice slip not found."
+private const val testErrorJson = "{\"message\": {\"type\": \"$errorType\", \"text\": \"$errorText\"}}"
 
 class AdviceServiceTest {
 
@@ -40,6 +47,28 @@ class AdviceServiceTest {
 
         val testObserver = adviceService.getRandomAdvice().test()
         testObserver.assertValue(AdviceObject(SlipObject(id, advice)))
+    }
+
+    @Test
+    fun getAdviceByIdWithValidId_EmitsAdvice() {
+        mockWebServer.enqueue(
+                MockResponse()
+                        .setBody(testJson)
+                        .setResponseCode(200))
+
+        val testObserver = adviceService.getAdviceById(id).test()
+        testObserver.assertValue(AdviceObject(SlipObject(id, advice)))
+    }
+
+    @Test
+    fun getAdviceByIdWithInvalidId_EmitsAdvice() {
+        mockWebServer.enqueue(
+                MockResponse()
+                        .setBody(testErrorJson)
+                        .setResponseCode(200))
+
+        val testObserver = adviceService.getAdviceById(invalidId).test()
+        testObserver.assertValue(AdviceObject(error = ErrorMessage(errorType, errorText)))
     }
 
 }
